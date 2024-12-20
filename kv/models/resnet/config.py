85055e9bfc954cfc3a8d7af6aabb52c2633b6978
IMvision12 <gitesh.ch.0912@gmail.com>
2024-12-19 17:45:43 -0800

    Refactor ResNet Models

--- kv/models/resnet/config.py
+++ kv/models/resnet/config.py
@@ -1,83 +1,18 @@
 RESNET_MODEL_CONFIG = {
-    "resnet50": {
-        "block_type": "bottleneck",
+    "ResNet50": {
+        "block_fn": "bottleneck_block",
         "block_repeats": [3, 4, 6, 3],
         "filters": [64, 128, 256, 512],
     },
-    "resnet101": {
-        "block_type": "bottleneck",
+    "ResNet101": {
+        "block_fn": "bottleneck",
         "block_repeats": [3, 4, 23, 3],
         "filters": [64, 128, 256, 512],
     },
-    "resnet152": {
-        "block_type": "bottleneck",
+    "ResNet152": {
+        "block_fn": "bottleneck",
         "block_repeats": [3, 8, 36, 3],
         "filters": [64, 128, 256, 512],
-    },
-    "resnext50_32x4d": {
-        "block_type": "resnext",
-        "block_repeats": [3, 4, 6, 3],
-        "filters": [64, 128, 256, 512],
-        "groups": 32,
-        "width_factor": 2,
-    },
-    "resnext101_32x4d": {
-        "block_type": "resnext",
-        "block_repeats": [3, 4, 23, 3],
-        "filters": [64, 128, 256, 512],
-        "groups": 32,
-        "width_factor": 2,
-    },
-    "resnext101_32x8d": {
-        "block_type": "resnext",
-        "block_repeats": [3, 4, 23, 3],
-        "filters": [64, 128, 256, 512],
-        "groups": 32,
-        "width_factor": 4,
-    },
-    "resnext101_32x16d": {
-        "block_type": "resnext",
-        "block_repeats": [3, 4, 23, 3],
-        "filters": [64, 128, 256, 512],
-        "groups": 32,
-        "width_factor": 8,
-    },
-    "resnext101_32x32d": {
-        "block_type": "resnext",
-        "block_repeats": [3, 4, 23, 3],
-        "filters": [64, 128, 256, 512],
-        "groups": 32,
-        "width_factor": 16,
-    },
-    "seresnet50": {
-        "block_type": "bottleneck",
-        "block_repeats": [3, 4, 6, 3],
-        "filters": [64, 128, 256, 512],
-        "senet": True,
-    },
-    "seresnext50_32x4d": {
-        "block_type": "resnext",
-        "block_repeats": [3, 4, 6, 3],
-        "filters": [64, 128, 256, 512],
-        "groups": 32,
-        "width_factor": 2,
-        "senet": True,
-    },
-    "seresnext101_32x4d": {
-        "block_type": "resnext",
-        "block_repeats": [3, 4, 23, 3],
-        "filters": [64, 128, 256, 512],
-        "groups": 32,
-        "width_factor": 2,
-        "senet": True,
-    },
-    "seresnext101_32x8d": {
-        "block_type": "resnext",
-        "block_repeats": [3, 4, 23, 3],
-        "filters": [64, 128, 256, 512],
-        "groups": 32,
-        "width_factor": 4,
-        "senet": True,
     },
 }
 
@@ -115,88 +50,5 @@ RESNET_WEIGHTS_CONFIG = {
         "gluon_in1k": {
             "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnet50_a1_in1k.keras",
         },
-    },
-    # ResNeXt Variants
-    "ResNeXt50_32x4d": {
-        "a1_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext50_32x4d_a1_in1k.keras",
-        },
-        "tv_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext50_32x4d_tv_in1k.keras",
-        },
-        "gluon_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnet50_a1_in1k.keras",
-        },
-        "fb_ssl_yfcc100m_ft_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext50_32x4d_fb_ssl_yfcc100m_ft_in1k.keras",
-        },
-        "fb_swsl_ig1b_ft_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext50_32x4d_fb_swsl_ig1b_ft_in1k.keras",
-        },
-    },
-    "ResNeXt101_32x4d": {
-        "fb_ssl_yfcc100m_ft_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext101_32x4d_fb_ssl_yfcc100m_ft_in1k.keras",
-        },
-        "fb_swsl_ig1b_ft_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext101_32x4d_fb_swsl_ig1b_ft_in1k.keras",
-        },
-        "gluon_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnet50_a1_in1k.keras",
-        },
-    },
-    "ResNeXt101_32x8d": {
-        "tv_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext101_32x8d_tv_in1k.keras",
-        },
-        "fb_wsl_ig1b_ft_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext101_32x8d_fb_wsl_ig1b_ft_in1k.keras",
-        },
-        "fb_ssl_yfcc100m_ft_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext101_32x8d_fb_ssl_yfcc100m_ft_in1k.keras",
-        },
-        "fb_swsl_ig1b_ft_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext101_32x8d_fb_swsl_ig1b_ft_in1k.keras",
-        },
-    },
-    "ResNeXt101_32x16d": {
-        "fb_wsl_ig1b_ft_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext101_32x16d_fb_wsl_ig1b_ft_in1k.keras",
-        },
-        "fb_ssl_yfcc100m_ft_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext101_32x16d_fb_ssl_yfcc100m_ft_in1k.keras",
-        },
-        "fb_swsl_ig1b_ft_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext101_32x16d_fb_swsl_ig1b_ft_in1k.keras",
-        },
-    },
-    "ResNeXt101_32x32d": {
-        "fb_wsl_ig1b_ft_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnext101_32x32d_fb_wsl_ig1b_ft_in1k.keras",
-        }
-    },
-    # SE-ResNet and SE-ResNeXt Variants
-    "SEResNet50": {
-        "a1_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/seresnet50_a1_in1k.keras",
-        }
-    },
-    "SEResNeXt50_32x4d": {
-        "racm_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/seresnext50_32x4d_racm_in1k.keras",
-        },
-        "gluon_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnet50_a1_in1k.keras",
-        },
-    },
-    "SEResNeXt101_32x4d": {
-        "gluon_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/resnet50_a1_in1k.keras",
-        },
-    },
-    "SEResNeXt101_32x8d": {
-        "ah_in1k": {
-            "url": "https://github.com/IMvision12/keras-vision/releases/download/v0.1/seresnext101_32x8d_ah_in1k.keras",
-        }
     },
 }
--- kv/models/resnext/convert_resnext_torch_to_keras.py
+++ kv/models/resnext/convert_resnext_torch_to_keras.py
@@ -5,7 +5,7 @@ import timm
 import torch
 from tqdm import tqdm
 
-from kv.models.resnet import ResNet50
+from kv.models.resnext import ResNeXt50_32x4d
 from kv.utils.custom_exception import WeightMappingError, WeightShapeMismatchError
 from kv.utils.test_keras_models import run_all_tests
 from kv.utils.weight_split_torch_keras import split_model_weights
@@ -33,8 +33,8 @@ weight_name_mapping: Dict[str, str] = {
 }
 
 model_config: Dict[str, Union[type, str, List[int], int, bool]] = {
-    "keras_model_cls": SEResNeXt50_32x4d,
-    "torch_model_name": "seresnext50_32x4d",
+    "keras_model_cls": ResNeXt50_32x4d,
+    "torch_model_name": "resnext50_32x4d.gluon_in1k",
     "input_shape": [224, 224, 3],
     "num_classes": 1000,
     "include_top": True,

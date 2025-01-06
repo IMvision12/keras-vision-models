8cc195785a381edd1e26e2d001b4bab8020169f6
IMvision12 <gitesh.ch.0912@gmail.com>
2025-01-05 23:55:41 -0800

    Add ImagePreprocessingLayer

--- kv/layers/__init__.py
+++ kv/layers/__init__.py
@@ -1,5 +1,6 @@
 from .class_dist_token import ClassDistToken
 from .global_response_norm import GlobalResponseNorm
+from .image_preprocessing import ImagePreprocessingLayer
 from .layer_scale import LayerScale
 from .multi_head_self_attention import MultiHeadSelfAttention
 from .pos_embedding import AddPositionEmbs

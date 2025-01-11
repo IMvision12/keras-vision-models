3c37a28fa94b3650dc190b77bfc0a14c9d4e6a9d
IMvision12 <88665786+IMvision12@users.noreply.github.com>
2025-01-11 00:01:25 -0800

    Add Efficientnet Lite (#22)

--- kv/models/efficientnet_lite/convert_efficietnet_lite_torch_to_keras.py
+++ kv/models/efficientnet_lite/convert_efficietnet_lite_torch_to_keras.py
@@ -1,4 +1,3 @@
-import re
 from typing import Any, Dict, List, Optional, Union
 
 import keras
@@ -6,7 +5,7 @@ import timm
 import torch
 from tqdm import tqdm
 
-from kv.models.efficientnet import EfficientNetB0
+from kv.models import EfficientNetLite4
 from kv.utils.custom_exception import WeightMappingError, WeightShapeMismatchError
 from kv.utils.model_equivalence_tester import verify_model_equivalence
 from kv.utils.weight_split_torch_and_keras import split_model_weights
@@ -15,41 +14,34 @@ from kv.utils.weight_transfer_torch_to_keras import (
     transfer_weights,
 )
 
-block_mappings = {}
-
-for i in range(6):
-    block_prefix = f"blocks.0.{i}"
-    # Conv layer mappings
-    block_mappings[f"{block_prefix}.conv_pwl"] = f"{block_prefix}.conv_pw"
-    # BN layer mappings
-    block_mappings[f"{block_prefix}.bn2"] = f"{block_prefix}.bn1"
-    block_mappings[f"{block_prefix}.bn3"] = f"{block_prefix}.bn2"
-
-base_mappings = {
-    "_kernel": ".weight",
-    "_gamma": ".weight",
-    "_beta": ".bias",
-    "_bias": ".bias",
-    "_moving_mean": ".running_mean",
-    "_moving_variance": ".running_var",
-    "se_": "se.",
-    "batchnorm_1": "bn1",
-    "batchnorm_2": "bn2",
-    "batchnorm_3": "bn3",
-    "conv2d_1": "conv_pw",
+weight_name_mapping = {
+    "_": ".",
+    "conv.stem": "conv_stem",
+    "conv2d.1": "conv_pw",
     "dwconv2d": "conv_dw",
-    "conv2d_2": "conv_pwl",
+    "conv2d.2": "conv_pwl",
+    "batchnorm.1": "bn1",
+    "batchnorm.2": "bn2",
+    "batchnorm.3": "bn3",
+    "blocks.0.0.bn2": "blocks.0.0.bn1",
+    "blocks.0.0.bn3": "blocks.0.0.bn2",
+    "blocks.0.0.conv_pwl": "blocks.0.0.conv_pw",
+    "conv.head": "conv_head",
+    "kernel": "weight",
+    "gamma": "weight",
+    "beta": "bias",
+    "bias": "bias",
+    "moving.mean": "running_mean",
+    "moving.variance": "running_var",
     "predictions": "classifier",
 }
 
-weight_name_mapping = {**base_mappings, **block_mappings}
-
 model_config: Dict[str, Union[type, str, List[int], int, bool]] = {
-    "keras_model_cls": EfficientNetB0,
-    "torch_model_name": "tf_efficientnet_b0.ns_jft_in1k",
+    "keras_model_cls": EfficientNetLite4,
+    "torch_model_name": "tf_efficientnet_lite4",
     "input_shape": [
-        224,
-        224,
+        380,
+        380,
         3,
     ],  # Change as per the default given for different models
     "num_classes": 1000,
@@ -112,11 +104,6 @@ for keras_weight, keras_weight_name in tqdm(
 ):
     torch_weight_name: str = keras_weight_name
     for keras_name_part, torch_name_part in weight_name_mapping.items():
-        torch_weight_name = re.sub(
-            r"blocks_(\d+)_(\d+)_",
-            lambda m: f"blocks.{m.group(1)}.{m.group(2)}.",
-            torch_weight_name,
-        )
         torch_weight_name = torch_weight_name.replace(keras_name_part, torch_name_part)
 
     torch_weights_dict: Dict[str, torch.Tensor] = {
@@ -142,7 +129,7 @@ for keras_weight, keras_weight_name in tqdm(
 results = verify_model_equivalence(
     model_a=torch_model,
     model_b=keras_model,
-    input_shape=(224, 224, 3),
+    input_shape=(380, 380, 3),
     output_specs={"num_classes": 1000},
     run_performance=False,
 )

fd7d160e549c98e9ee8ebb2d73b4c5b963e7ed88
IMvision12 <88665786+IMvision12@users.noreply.github.com>
2025-01-29 17:19:43 -0800

    Add InceptionNeXt Model (#38)

--- kv/models/classification/__init__.py
+++ kv/models/classification/__init__.py
@@ -7,6 +7,7 @@ from .efficientnet import *
 from .efficientnet_lite import *
 from .efficientnetv2 import *
 from .flexivit import *
+from .inception_next import *
 from .inception_resnetv2 import *
 from .inceptionv3 import *
 from .inceptionv4 import *
---
+++ kv/models/classification/inception_next/__init__.py
@@ -0,0 +1,1 @@
+from .inception_next_model import *

84a514ee7fec86be9b97748d6ef654cab630de66
IMvision12 <gitesh.ch.0912@gmail.com>
2025-02-09 18:59:43 -0800

    Add config

---
+++ kv/models/mobilenetv3/config.py
@@ -0,0 +1,66 @@
+MOBILENETV3_MODEL_CONFIG = {
+    "MobileNetV3Small075": {
+        "width_multiplier": 0.75,
+        "depth_multiplier": 1.0,
+        "fix_channels": False,
+        "config": "small",
+        "minimal": False,
+    },
+    "MobileNetV3Small100": {
+        "width_multiplier": 1.0,
+        "depth_multiplier": 1.0,
+        "fix_channels": False,
+        "config": "small",
+        "minimal": False,
+    },
+    "MobileNetV3SmallMinimal100": {
+        "width_multiplier": 1.0,
+        "depth_multiplier": 1.0,
+        "fix_channels": False,
+        "config": "small",
+        "minimal": True,
+    },
+    "MobileNetV3Large75": {
+        "width_multiplier": 0.75,
+        "depth_multiplier": 1.0,
+        "fix_channels": False,
+        "config": "large",
+        "minimal": False,
+    },
+    "MobileNetV3Large100": {
+        "width_multiplier": 1.0,
+        "depth_multiplier": 1.0,
+        "fix_channels": False,
+        "config": "large",
+        "minimal": False,
+    },
+    "MobileNetV3LargeMinimal100": {
+        "width_multiplier": 1.0,
+        "depth_multiplier": 1.0,
+        "fix_channels": False,
+        "config": "large",
+        "minimal": True,
+    },
+}
+
+
+MOBILENETV3_WEIGHTS_CONFIG = {
+    "MobileNetV3Small075": {
+        "in1k": {"url": ""},
+    },
+    "MobileNetV3Small100": {
+        "in1k": {"url": ""},
+    },
+    "MobileNetV3SmallMinimal100": {
+        "in1k": {"url": ""},
+    },
+    "MobileNetV3Large75": {
+        "in1k": {"url": ""},
+    },
+    "MobileNetV3Large100": {
+        "in1k": {"url": ""},
+    },
+    "MobileNetV3LargeMinimal100": {
+        "in1k": {"url": ""},
+    },
+}

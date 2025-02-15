6c60d2bc550a892d58835a27c7436d81b5c53d4d
IMvision12 <gitesh.ch.0912@gmail.com>
2025-02-14 19:03:15 -0800

    Add test for resnet family

---
+++ kv/models/senet/senet_test.py
@@ -0,0 +1,18 @@
+import pytest
+
+from kv.models.senet import SEResNeXt50_32x4d
+from kv.tests.test_modeling import BaseVisionTest, ModelConfig
+
+
+class TestSeNet(BaseVisionTest):
+    @pytest.fixture
+    def model_config(self) -> ModelConfig:
+        return ModelConfig(model_cls=SEResNeXt50_32x4d, input_shape=(224, 224, 3))
+
+    def get_default_kwargs(self) -> dict:
+        return {
+            "include_normalization": True,
+            "normalization_mode": "imagenet",
+            "classifier_activation": "softmax",
+            "weights": None,
+        }
---
+++ kv/models/vgg/vgg_test.py
@@ -0,0 +1,18 @@
+import pytest
+
+from kv.models.vgg import VGG16
+from kv.tests.test_modeling import BaseVisionTest, ModelConfig
+
+
+class TestVGG(BaseVisionTest):
+    @pytest.fixture
+    def model_config(self) -> ModelConfig:
+        return ModelConfig(model_cls=VGG16, input_shape=(224, 224, 3))
+
+    def get_default_kwargs(self) -> dict:
+        return {
+            "include_normalization": True,
+            "normalization_mode": "imagenet",
+            "classifier_activation": "softmax",
+            "weights": None,
+        }

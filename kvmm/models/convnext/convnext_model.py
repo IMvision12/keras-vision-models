2b61a80931578d8db0b76f3eb08cc3bddf0113f7
IMvision12 <gitesh.ch.0912@gmail.com>
2025-02-17 23:33:59 -0800

    fix convnext and convmixer

--- kvmm/models/convnext/convnext_model.py
+++ kvmm/models/convnext/convnext_model.py
@@ -188,7 +188,7 @@ class ConvNeXt(keras.Model):
             )
 
         data_format = keras.config.image_data_format()
-        channels_axis = -1 if data_format == "channels_last" else -3
+        channels_axis = -1 if data_format == "channels_last" else -1
 
         input_shape = imagenet_utils.obtain_input_shape(
             input_shape,

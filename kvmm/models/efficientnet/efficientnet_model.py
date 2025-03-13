160f32b939e6c957520feff1ecb774037f467449
IMvision12 <gitesh.ch.0912@gmail.com>
2025-03-13 00:37:39 -0700

    Format

--- kvmm/models/efficientnet/efficientnet_model.py
+++ kvmm/models/efficientnet/efficientnet_model.py
@@ -271,7 +271,6 @@ class EfficientNet(keras.Model):
         data_format = keras.config.image_data_format()
         channels_axis = -1 if data_format == "channels_last" else 1
 
-
         input_shape = imagenet_utils.obtain_input_shape(
             input_shape,
             default_size=475 if weights and "475" in weights else default_size,

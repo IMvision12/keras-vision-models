548b219d6b2f2621916f17787ab99f66cf8e2385
IMVision12 <gitesh.ch.0912@gmail.com>
2025-03-25 00:32:12 -0700

    fix

--- kvmm/models/backbones/deit/deit_test.py
+++ kvmm/models/backbones/deit/deit_test.py
@@ -1,13 +1,13 @@
 import pytest
 
-from kvmm.models.deit import DEiTTinyDistilled16
+from kvmm.models import deit
 from kvmm.tests.test_modeling import BaseVisionTest, ModelConfig
 
 
 class TestDEiT(BaseVisionTest):
     @pytest.fixture
     def model_config(self) -> ModelConfig:
-        return ModelConfig(model_cls=DEiTTinyDistilled16, input_shape=(32, 32, 3))
+        return ModelConfig(model_cls=deit.DEiTTinyDistilled16, input_shape=(32, 32, 3))
 
     def get_default_kwargs(self) -> dict:
         return {

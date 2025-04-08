be97fa920f21b608a5c9dd6b4b3e8fb963cfa515
IMVision12 <gitesh.ch.0912@gmail.com>
2025-04-08 14:47:31 -0700

    Fix config

--- kvmm/models/backbones/resnetv2/config.py
+++ kvmm/models/backbones/resnetv2/config.py
@@ -1,60 +1,31 @@
 RESNETV2_MODEL_CONFIG = {
-    "ResNetV2_50": {
-        "block_repeats": [3, 4, 6, 3],
-        "width_factor": 1,
-        "use_batchnorm": True,
-        "use_conv": True,
-    },
     "ResNetV2_50x1": {
         "block_repeats": [3, 4, 6, 3],
         "width_factor": 1,
-        "use_batchnorm": False,
-        "use_conv": False,
     },
     "ResNetV2_50x3": {
         "block_repeats": [3, 4, 6, 3],
         "width_factor": 3,
-        "use_batchnorm": False,
-        "use_conv": False,
-    },
-    "ResNetV2_101": {
-        "block_repeats": [3, 4, 23, 3],
-        "width_factor": 1,
-        "use_batchnorm": True,
-        "use_conv": True,
     },
     "ResNetV2_101x1": {
         "block_repeats": [3, 4, 23, 3],
         "width_factor": 1,
-        "use_batchnorm": False,
-        "use_conv": False,
     },
     "ResNetV2_101x3": {
         "block_repeats": [3, 4, 23, 3],
         "width_factor": 3,
-        "use_batchnorm": False,
-        "use_conv": False,
     },
     "ResNetV2_152x2": {
         "block_repeats": [3, 8, 36, 3],
         "width_factor": 2,
-        "use_batchnorm": False,
-        "use_conv": False,
     },
     "ResNetV2_152x4": {
         "block_repeats": [3, 8, 36, 3],
         "width_factor": 4,
-        "use_batchnorm": False,
-        "use_conv": False,
     },
 }
 
 RESNETV2_WEIGHTS_CONFIG = {
-    "ResNetV2_50": {
-        "a1h_in1k_224": {
-            "url": "",
-        },
-    },
     "ResNetV2_50x1": {
         "goog_in21k_ft_in1k_448": {
             "url": "",
@@ -68,11 +39,6 @@ RESNETV2_WEIGHTS_CONFIG = {
             "url": "",
         },
         "goog_in21k_224": {
-            "url": "",
-        },
-    },
-    "ResNetV2_101": {
-        "a1h_in1k_224": {
             "url": "",
         },
     },

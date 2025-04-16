3718efc9ebd42319385f5f992816e37f2675c594
IMVision12 <gitesh.ch.0912@gmail.com>
2025-04-16 14:58:58 -0700

    fix

--- tests/models/backbones/convmixer/convmixer_test.py
+++ tests/models/backbones/convmixer/convmixer_test.py
@@ -1,7 +1,7 @@
 import pytest
 
 from kvmm.models import convmixer
-from tests.test_backbone_modeling import BackboneTest, ModelConfig
+from ....test_backbone_modeling import BackboneTest, ModelConfig
 
 
 class TestConvMixer(BackboneTest):

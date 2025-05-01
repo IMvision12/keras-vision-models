6be92bb2976c6c02499485528c196205fb2d3145
IMVision12 <gitesh.ch.0912@gmail.com>
2025-05-01 13:12:48 -0700

    Fix segmentation test

--- tests/models/segmentation/segformer/segformer_test.py
+++ tests/models/segmentation/segformer/segformer_test.py
@@ -2,18 +2,29 @@ import pytest
 
 from kvmm.models import segformer
 
-from ....test_segmentation_modeling import (
-    SegmentationModelConfig,
-    SegmentationTest,
-)
+from ....test_segmentation_modeling import SegmentationTestCase
 
 
-class TestSegFormer(SegmentationTest):
-    @pytest.fixture
-    def model_config(self) -> SegmentationModelConfig:
-        return SegmentationModelConfig(
-            model_cls=segformer.SegFormerB0, input_shape=(32, 32, 3)
+class TestSegFormer(SegmentationTestCase):
+    """Test case for the SegFormer model."""
+
+    __test__ = True
+
+    def setUp(self):
+        super().setUp()
+        self.configure(model_cls=segformer.SegFormerB0, input_shape=(32, 32, 3))
+
+    def get_default_kwargs(self) -> dict:
+        return {
+            "weights": None,
+        }
+
+    def test_weight_loading(self):
+        custom_model = segformer.SegFormerB0(
+            input_shape=(32, 32, 3),
+            weights="ade20k_512",
         )
-
-    def test_auxiliary_outputs(self, model_config):
+        return super().test_weight_loading(custom_model)
+    
+    def test_auxiliary_outputs(self):
         pytest.skip("SegFormer doesn't produce auxiliary outputs")

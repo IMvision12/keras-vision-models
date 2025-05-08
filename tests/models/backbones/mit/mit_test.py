aa5ccc297a9be7e551651dca13911fcbcbe92081
IMVision12 <gitesh.ch.0912@gmail.com>
2025-05-08 14:55:15 -0700

    Update mit and mixer

--- tests/models/backbones/mit/mit_test.py
+++ tests/models/backbones/mit/mit_test.py
@@ -1,27 +1,33 @@
 from kvmm.models import mit
+from keras import ops
+from ....test_modelling import ModelTestCase
 
-from ....test_backbone_modeling import BackboneTestCase
-
-
-class TestMixTransformer(BackboneTestCase):
-    """Test case for the MixTransformer model."""
-
+class TestMixTransformer(ModelTestCase):
     __test__ = True
-
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
     def setUp(self):
         super().setUp()
-        self.configure(model_cls=mit.MiT_B0, input_shape=(32, 32, 3))
-
-    def get_default_kwargs(self) -> dict:
-        return {
-            "include_normalization": True,
-            "normalization_mode": "imagenet",
-            "classifier_activation": "softmax",
-            "weights": None,
-        }
-
-    def test_weight_loading(self):
+        
+        self.input_data = ops.ones((2, 32, 32, 3))
+        self.expected_output_shape = (2, 1000)
+        
+        self.configure(
+            model_cls=mit.MiT_B0,
+            model_type="backbone",
+            init_kwargs={
+                "weights": None,
+                "input_shape": (32, 32, 3),
+                "include_top": True,
+            },
+            input_data=self.input_data,
+            expected_output_shape=self.expected_output_shape
+        )
+    
+    def test_weight_initialization(self):
         custom_model = mit.MiT_B0(
             input_shape=(32, 32, 3),
         )
-        return super().test_weight_loading(custom_model)
+        return super().test_weight_initialization(custom_model)
--- tests/models/backbones/mlp_mixer/mlp_mixer_test.py
+++ tests/models/backbones/mlp_mixer/mlp_mixer_test.py
@@ -1,27 +1,33 @@
 from kvmm.models import mlp_mixer
+from keras import ops
+from ....test_modelling import ModelTestCase
 
-from ....test_backbone_modeling import BackboneTestCase
-
-
-class TestMLPMixer(BackboneTestCase):
-    """Test case for the MLPMixer model."""
-
+class TestMLPMixer(ModelTestCase):
     __test__ = True
-
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
     def setUp(self):
         super().setUp()
-        self.configure(model_cls=mlp_mixer.MLPMixerB16, input_shape=(32, 32, 3))
-
-    def get_default_kwargs(self) -> dict:
-        return {
-            "include_normalization": True,
-            "normalization_mode": "imagenet",
-            "classifier_activation": "softmax",
-            "weights": None,
-        }
-
-    def test_weight_loading(self):
-        custom_model = mlp_mixer.MLPMixerB16(
-            input_shape=(224, 224, 3),
+        
+        self.input_data = ops.ones((2, 32, 32, 3))
+        self.expected_output_shape = (2, 1000)
+        
+        self.configure(
+            model_cls=mlp_mixer.MLPMixerB16,
+            model_type="backbone",
+            init_kwargs={
+                "weights": None,
+                "input_shape": (32, 32, 3),
+                "include_top": True,
+            },
+            input_data=self.input_data,
+            expected_output_shape=self.expected_output_shape
         )
-        return super().test_weight_loading(custom_model)
+    
+    def test_weight_initialization(self):
+        custom_model = mlp_mixer.MLPMixerB16(
+            input_shape=(32, 32, 3),
+        )
+        return super().test_weight_initialization(custom_model)

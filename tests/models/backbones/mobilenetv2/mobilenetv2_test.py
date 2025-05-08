3e9d66f9ead7c0cd18e293b0243d0d05c6b7a5b9
IMVision12 <gitesh.ch.0912@gmail.com>
2025-05-08 14:57:07 -0700

    Update mobilenet and mobilevit

--- tests/models/backbones/mobilenetv2/mobilenetv2_test.py
+++ tests/models/backbones/mobilenetv2/mobilenetv2_test.py
@@ -1,27 +1,33 @@
 from kvmm.models import mobilenetv2
+from keras import ops
+from ....test_modelling import ModelTestCase
 
-from ....test_backbone_modeling import BackboneTestCase
-
-
-class TestMobileNetV2(BackboneTestCase):
-    """Test case for the MobileNetV2 model."""
-
+class TestMobileNetV2(ModelTestCase):
     __test__ = True
-
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
     def setUp(self):
         super().setUp()
-        self.configure(model_cls=mobilenetv2.MobileNetV2WM50, input_shape=(32, 32, 3))
-
-    def get_default_kwargs(self) -> dict:
-        return {
-            "include_normalization": True,
-            "normalization_mode": "imagenet",
-            "classifier_activation": "softmax",
-            "weights": None,
-        }
-
-    def test_weight_loading(self):
+        
+        self.input_data = ops.ones((2, 32, 32, 3))
+        self.expected_output_shape = (2, 1000)
+        
+        self.configure(
+            model_cls=mobilenetv2.MobileNetV2WM50,
+            model_type="backbone",
+            init_kwargs={
+                "weights": None,
+                "input_shape": (32, 32, 3),
+                "include_top": True,
+            },
+            input_data=self.input_data,
+            expected_output_shape=self.expected_output_shape
+        )
+    
+    def test_weight_initialization(self):
         custom_model = mobilenetv2.MobileNetV2WM50(
             input_shape=(32, 32, 3),
         )
-        return super().test_weight_loading(custom_model)
+        return super().test_weight_initialization(custom_model)

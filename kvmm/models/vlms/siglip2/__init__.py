b630ba739bed2fd337df7f2bac126385e2ee7b29
IMvision12 <gitesh.ch.0912@gmail.com>
2025-06-20 14:29:56 -0700

    Fix inits

---
+++ kvmm/models/vlms/siglip2/__init__.py
@@ -0,0 +1,9 @@
+from kvmm.models.vlms.siglip2.siglip2_model import (
+    SigLIP2BaseP16,
+    SigLIP2BaseP32,
+    SigLIP2GiantP16,
+    SigLIP2LargeP16,
+    SigLIP2So400mP14,
+    SigLIP2So400mP16,
+)
+

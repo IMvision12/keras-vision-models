78b8979459b23604dcc2940cad674ee24e99c8f9
IMvision12 <gitesh.ch.0912@gmail.com>
2025-09-03 01:03:16 -0700

    Add yolov8 config file

---
+++ kvmm/models/yolo/yolov8/config.py
@@ -0,0 +1,55 @@
+YOLOV8_MODEL_CONFIG = {
+    "YoloV8n": {
+        "depth_multiple": 0.33,
+        "width_multiple": 0.25,
+        "max_channels": 1024,
+    },
+    "YoloV8s": {
+        "depth_multiple": 0.33,
+        "width_multiple": 0.50,
+        "max_channels": 1024,
+    },
+    "YoloV8m": {
+        "depth_multiple": 0.67,
+        "width_multiple": 0.75,
+        "max_channels": 768,
+    },
+    "YoloV8l": {
+        "depth_multiple": 1.00,
+        "width_multiple": 1.00,
+        "max_channels": 512,
+    },
+    "YoloV8x": {
+        "depth_multiple": 1.00,
+        "width_multiple": 1.25,
+        "max_channels": 512,
+    },
+}
+
+YOLOV8_WEIGHTS_CONFIG = {
+    "YoloV8n": {
+        "coco": {
+            "url": "",
+        },
+    },
+    "YoloV8s": {
+        "coco": {
+            "url": "",
+        },
+    },
+    "YoloV8m": {
+        "coco": {
+            "url": "",
+        },
+    },
+    "YoloV8l": {
+        "coco": {
+            "url": "",
+        },
+    },
+    "YoloV8x": {
+        "coco": {
+            "url": "",
+        },
+    },
+}

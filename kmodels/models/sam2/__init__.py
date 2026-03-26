fbc5a5834b054c4d455505ec8179024f218627f1
IMvision12 <gitesh.ch.0912@gmail.com>
2026-03-26 12:51:18 -0700

    Add SAM 2

---
+++ kmodels/models/sam2/__init__.py
@@ -0,0 +1,6 @@
+from kmodels.models.sam2.sam2_model import (
+    SAM2_Hiera_Base_Plus,
+    SAM2_Hiera_Large,
+    SAM2_Hiera_Small,
+    SAM2_Hiera_Tiny,
+)
---
+++ kmodels/models/sam2/config.py
@@ -0,0 +1,46 @@
+SAM2_MODEL_CONFIG = {
+    "SAM2_Hiera_Tiny": {
+        "hidden_size": 96,
+        "blocks_per_stage": [1, 2, 7, 2],
+        "embed_dim_per_stage": [96, 192, 384, 768],
+        "num_attention_heads_per_stage": [1, 2, 4, 8],
+        "window_size_per_stage": [8, 4, 14, 7],
+        "global_attention_blocks": [5, 7, 9],
+        "backbone_channel_list": [768, 384, 192, 96],
+    },
+    "SAM2_Hiera_Small": {
+        "hidden_size": 96,
+        "blocks_per_stage": [1, 2, 11, 2],
+        "embed_dim_per_stage": [96, 192, 384, 768],
+        "num_attention_heads_per_stage": [1, 2, 4, 8],
+        "window_size_per_stage": [8, 4, 14, 7],
+        "global_attention_blocks": [7, 10, 13],
+        "backbone_channel_list": [768, 384, 192, 96],
+    },
+    "SAM2_Hiera_Base_Plus": {
+        "hidden_size": 112,
+        "blocks_per_stage": [2, 3, 16, 3],
+        "embed_dim_per_stage": [112, 224, 448, 896],
+        "num_attention_heads_per_stage": [2, 4, 8, 16],
+        "window_size_per_stage": [8, 4, 14, 7],
+        "global_attention_blocks": [12, 16, 20],
+        "backbone_channel_list": [896, 448, 224, 112],
+        "window_pos_embed_bg_size": [14, 14],
+    },
+    "SAM2_Hiera_Large": {
+        "hidden_size": 144,
+        "blocks_per_stage": [2, 6, 36, 4],
+        "embed_dim_per_stage": [144, 288, 576, 1152],
+        "num_attention_heads_per_stage": [2, 4, 8, 16],
+        "window_size_per_stage": [8, 4, 16, 8],
+        "global_attention_blocks": [23, 33, 43],
+        "backbone_channel_list": [1152, 576, 288, 144],
+    },
+}
+
+SAM2_WEIGHTS_CONFIG = {
+    "SAM2_Hiera_Tiny": {},
+    "SAM2_Hiera_Small": {},
+    "SAM2_Hiera_Base_Plus": {},
+    "SAM2_Hiera_Large": {},
+}

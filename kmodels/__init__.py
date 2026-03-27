b4afa299584d85a33ec60ad63687d9ce2aea8b59
Gitesh <88665786+IMvision12@users.noreply.github.com>
2026-03-27 02:26:10 -0700

    Release 0.2.1 (#154)

--- kmodels/__init__.py
+++ kmodels/__init__.py
@@ -2,4 +2,4 @@ from kmodels import layers, models, utils
 from kmodels.model_registry import list_models, register_model
 from kmodels.version import version
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"

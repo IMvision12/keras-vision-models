cb9ebb326e3e1f2f0068405840fee92f492afbdc
IMvision12 <gitesh.ch.0912@gmail.com>
2026-04-03 12:59:04 -0700

    links

--- kmodels/models/swinv2/config.py
+++ kmodels/models/swinv2/config.py
@@ -60,57 +60,57 @@ SWINV2_MODEL_CONFIG = {
 SWINV2_WEIGHTS_CONFIG = {
     "SwinV2TinyW8": {
         "ms_in1k": {
-            "url": "",
+            "url": "https://github.com/IMvision12/keras-models/releases/download/swin/swinv2_tiny_window8_256_ms_in1k.weights.h5",
         },
     },
     "SwinV2TinyW16": {
         "ms_in1k": {
-            "url": "",
+            "url": "https://github.com/IMvision12/keras-models/releases/download/swin/swinv2_tiny_window16_256_ms_in1k.weights.h5",
         },
     },
     "SwinV2SmallW8": {
         "ms_in1k": {
-            "url": "",
+            "url": "https://github.com/IMvision12/keras-models/releases/download/swin/swinv2_small_window8_256_ms_in1k.weights.h5",
         },
     },
     "SwinV2SmallW16": {
         "ms_in1k": {
-            "url": "",
+            "url": "https://github.com/IMvision12/keras-models/releases/download/swin/swinv2_small_window16_256_ms_in1k.weights.h5",
         },
     },
     "SwinV2BaseW8": {
         "ms_in1k": {
-            "url": "",
+            "url": "https://github.com/IMvision12/keras-models/releases/download/swin/swinv2_base_window8_256_ms_in1k.weights.h5",
         },
     },
     "SwinV2BaseW12": {
         "ms_in22k": {
-            "url": "",
+            "url": "https://github.com/IMvision12/keras-models/releases/download/swin/swinv2_base_window12_192_ms_in22k.weights.h5",
         },
         "ms_in22k_ft_in1k_256": {
-            "url": "",
+            "url": "https://github.com/IMvision12/keras-models/releases/download/swin/swinv2_base_window12to16_192to256_ms_in22k_ft_in1k.weights.h5",
             "model_kwargs": {"window_size": 16, "pretrained_window_size": 12},
         },
         "ms_in22k_ft_in1k_384": {
-            "url": "",
+            "url": "https://github.com/IMvision12/keras-models/releases/download/swin/swinv2_base_window12to24_192to384_ms_in22k_ft_in1k.weights.h5",
             "model_kwargs": {"window_size": 24, "pretrained_window_size": 12},
         },
     },
     "SwinV2BaseW16": {
         "ms_in1k": {
-            "url": "",
+            "url": "https://github.com/IMvision12/keras-models/releases/download/swin/swinv2_base_window16_256_ms_in1k.weights.h5",
         },
     },
     "SwinV2LargeW12": {
         "ms_in22k": {
-            "url": "",
+            "url": "https://github.com/IMvision12/keras-models/releases/download/swin/swinv2_large_window12_192_ms_in22k.weights.h5",
         },
         "ms_in22k_ft_in1k_256": {
-            "url": "",
+            "url": "https://github.com/IMvision12/keras-models/releases/download/swin/swinv2_large_window12to16_192to256_ms_in22k_ft_in1k.weights.h5",
             "model_kwargs": {"window_size": 16, "pretrained_window_size": 12},
         },
         "ms_in22k_ft_in1k_384": {
-            "url": "",
+            "url": "https://github.com/IMvision12/keras-models/releases/download/swin/swinv2_large_window12to24_192to384_ms_in22k_ft_in1k.weights.h5",
             "model_kwargs": {"window_size": 24, "pretrained_window_size": 12},
         },
     },

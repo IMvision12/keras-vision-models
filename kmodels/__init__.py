a0566a94c59015d534a0e3f48473f4bfb5d8b734
IMvision12 <gitesh.ch.0912@gmail.com>
2026-04-18 12:13:18 -0700

    Release 0.3.0

--- kmodels/__init__.py
+++ kmodels/__init__.py
@@ -2,4 +2,4 @@ from kmodels import layers, models, utils, weight_utils
 from kmodels.model_registry import list_models, register_model
 from kmodels.version import version
 
-__version__ = "0.2.7"
+__version__ = "0.3.0"
